66751fedc1e040a15891694963db259da3d44895
trungnt9192 <trungnt9192@gmail.com>
2022-02-24 10:55:53 +0700

    upate

---
+++ js/resources.js
@@ -0,0 +1,47 @@
+// Resources - Q&A
+
+$(document).ready(function () {
+    $(".bt_1").click(function () {
+        $(".p_1").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_2").click(function () {
+        $(".p_2").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_3").click(function () {
+        $(".p_3").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_4").click(function () {
+        $(".p_4").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_5").click(function () {
+        $(".p_5").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_6").click(function () {
+        $(".p_6").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_7").click(function () {
+        $(".p_7").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_8").click(function () {
+        $(".p_8").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".bt_9").click(function () {
+        $(".p_9").toggle();
+    });
+});
